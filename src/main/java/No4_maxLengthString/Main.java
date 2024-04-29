3aaa9a05ecc13b21d4670547eba080501636c124
= <=>
2024-04-29 11:14:49 +0800

    solve No4

---
+++ src/main/java/No4_maxLengthString/Main.java
@@ -0,0 +1,36 @@
+package No4_maxLengthString;
+
+/**
+ * @Description TODO
+ * @Author tao.ling
+ * @Date 2024/4/28
+ **/
+public class Main {
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1)
+            return s.length();
+
+        int res = 0;
+        int begin = 0;
+        int subLen = 1;
+
+        while(subLen < s.length() - begin) {
+            String subStr = s.substring(begin, begin+subLen);
+
+            if (begin + subLen < s.length() && subStr.contains(String.valueOf(s.charAt(begin + subLen)))) {
+                res = res > subLen ? res : subLen;
+                begin = begin + 1;
+                subLen = 1;
+            }
+            else
+                ++subLen;
+        }
+
+        res = res > subLen ? res : subLen;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("dvdf"));
+    }
+}
